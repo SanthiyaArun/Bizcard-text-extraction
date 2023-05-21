ac07aa1e97b893088d8a87435290f74c5f878b17
SanthiyaArun <124423120+SanthiyaArun@users.noreply.github.com>
2023-05-21 13:38:43 +0530

    biz.py

--- biz.py
+++ biz.py
@@ -1,104 +1,87 @@
 import streamlit as st
 import psycopg2
 import easyocr
+import cv2
+import numpy as np
 
-# Load database credentials from st.secrets
-db_credentials = st.secrets["db_credentials"]
-
-# Connect to the database
+# Connect to PostgreSQL database
 try:
-    conn = psycopg2.connect(**db_credentials)
-    cur = conn.cursor()
-
-    # Create a table to store the extracted information
-    cur.execute('''CREATE TABLE IF NOT EXISTS business_cards
-                   (id SERIAL PRIMARY KEY,
-                    image BYTEA,
-                    company_name TEXT,
-                    card_holder_name TEXT,
-                    designation TEXT,
-                    mobile_number TEXT,
-                    email TEXT,
-                    website TEXT,
-                    area TEXT,
-                    city TEXT,
-                    state TEXT,
-                    pin_code TEXT);''')
-
-except (psycopg2.Error, KeyError) as e:
-    st.error("Error connecting to the database. Please check your credentials.")
+    connection = psycopg2.connect(
+        host="localhost",
+        user="postgres",
+        password="1204",
+        database="bizcardx"
+    )
+    cursor = connection.cursor()
+except psycopg2.Error as e:
+    st.error(f"Error connecting to the database: {e}")
     st.stop()
 
-# Function to extract information from the image
-def extract_info(image):
-    reader = easyocr.Reader(['en'])
-    results = reader.readtext(image)
-    extracted_info = {
-        'company_name': '',
-        'card_holder_name': '',
-        'designation': '',
-        'mobile_number': '',
-        'email': '',
-        'website': '',
-        'area': '',
-        'city': '',
-        'state': '',
-        'pin_code': ''
-    }
-    for result in results:
-        text = result[1].lower()
-        if 'company' in text:
-            extracted_info['company_name'] = result[1]
-        elif 'name' in text:
-            extracted_info['card_holder_name'] = result[1]
-        elif 'designation' in text:
-            extracted_info['designation'] = result[1]
-        elif 'mobile' in text:
-            extracted_info['mobile_number'] = result[1]
-        elif 'email' in text:
-            extracted_info['email'] = result[1]
-        elif 'website' in text:
-            extracted_info['website'] = result[1]
-        elif 'area' in text:
-            extracted_info['area'] = result[1]
-        elif 'city' in text:
-            extracted_info['city'] = result[1]
-        elif 'state' in text:
-            extracted_info['state'] = result[1]
-        elif 'pin' in text:
-            extracted_info['pin_code'] = result[1]
-    return extracted_info
+# Create a table to store the business card information
+create_table_query = """
+    CREATE TABLE IF NOT EXISTS bizcardx (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(255),
+        job_title VARCHAR(255),
+        address VARCHAR(255),
+        postcode VARCHAR(255),
+        phone VARCHAR(255),
+        email VARCHAR(255),
+        website VARCHAR(255),
+        company_name VARCHAR(225)
+    )
+"""
+cursor.execute(create_table_query)
+connection.commit()
 
-# Streamlit app code
-def app():
-    st.title('Business Card Reader')
-    uploaded_file = st.file_uploader('Upload a business card image')
-    if uploaded_file is not None:
-        image = uploaded_file.read()
-        st.image(uploaded_file, caption='Uploaded business card', use_column_width=True)
-        extracted_info = extract_info(image)
+# Create an OCR object to read text from the image
+reader = easyocr.Reader(['en'])
+
+# Streamlit app
+st.title("Business Card Extraction")
+
+# File upload
+uploaded_file = st.file_uploader("Upload a business card image", type=["jpg", "jpeg", "png"])
+
+# Handle user actions
+if uploaded_file is not None:
+    if st.button("Extract Information"):
+        # Read the image data
+        image = cv2.imdecode(np.fromstring(uploaded_file.read(), np.uint8), 1)
+
+        # Extract information from the image using OCR
+        bounds = reader.readtext(image, detail=0)
+        name = bounds[0] if len(bounds) > 0 else ""
+        job_title = bounds[1] if len(bounds) > 1 else ""
+        address = bounds[2] if len(bounds) > 2 else ""
+        postcode = bounds[3] if len(bounds) > 3 else ""
+        phone = bounds[4] if len(bounds) > 4 else ""
+        email = bounds[5] if len(bounds) > 5 else ""
+        website = bounds[6] if len(bounds) > 6 else ""
+        company_name = bounds[7] if len(bounds) > 7 else ""
 
         # Insert the extracted information into the database
-        insert_query = '''INSERT INTO business_cards (image, company_name, card_holder_name, designation, mobile_number, email, website, area, city, state, pin_code)
-                          VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);'''
-        try:
-            cur.execute(insert_query, (
-                psycopg2.Binary(image),
-                extracted_info['company_name'],
-                extracted_info['card_holder_name'],
-                extracted_info['designation'],
-                extracted_info['mobile_number'],
-                extracted_info['email'],
-                extracted_info['website'],
-                extracted_info['area'],
-                extracted_info['city'],
-                extracted_info['state'],
-                extracted_info['pin_code']
-            ))
-            conn.commit()
-            st.success('Information extracted and stored in the database.')
-        except psycopg2.Error as e:
-            st.error("Error inserting data into the database.")
-           
-if name == 'main':
-   app()
+        insert_query = """
+            INSERT INTO bizcardx (name, job_title, address, postcode, phone, email, website, company_name)
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+        """
+        values = (name, job_title, address, postcode, phone, email, website, company_name)
+        cursor.execute(insert_query, values)
+        connection.commit()
+
+        st.success("Business card information added to the database.")
+
+# Display the stored business card information
+display_cards = st.button("View Business Cards")
+if display_cards:
+    select_query = "SELECT * FROM bizcardx"
+    cursor.execute(select_query)
+    records = cursor.fetchall()
+    if len(records) > 0:
+        st.table(records)
+    else:
+        st.info("No business card records found.")
+
+# Close the database connection
+cursor.close()
+connection.close()
